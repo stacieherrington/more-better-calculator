c4aadf10dea9d0af903e4b2144979da3083490fd
Jade Helmet <thejadehelmet@gmail.com>
2019-02-01 20:38:00 -0600

    It has three working buttons.

---
+++ script.js
@@ -0,0 +1,16 @@
+let currentNumber = 0;
+
+for (let i = 7; i < 10; i++) {
+  document
+    .getElementById('number' + i)
+    .addEventListener('click', function () {     // Handle user input
+      currentNumber = (currentNumber * 10) + i;  // Change state
+      document                                   // Update view
+        .getElementById('display')               // Update view
+        .innerHTML = currentNumber;              // Update view
+    });
+}
+
+document
+  .getElementById('display')
+  .innerHTML = currentNumber;
