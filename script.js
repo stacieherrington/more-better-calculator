ca0a659373949a2fc982e966d0dbf551775b91cb
Jade Helmet <thejadehelmet@gmail.com>
2019-02-03 14:00:53 -0600

    The display clears on operator now.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let currentNumber = 0;
-let operator;
+let shouldClearDisplay = false;
 const operations = {
   plus: " + ",
   minus: " - ",
+  times: " &times; ",
+  divide: " &divide; ",
 };
-
-// operations.plus
-// operations["plus"]
-
-// const op = "minus";
-// operations[op]
 
 for (let i = 0; i < 10; i++) {
   document
     .getElementById('number' + i)
     .addEventListener('click', function () {     // Handle user input
+      if (shouldClearDisplay) {
+        currentNumber = 0;
+        shouldClearDisplay = false;
+      }
       currentNumber = (currentNumber * 10) + i;  // Change state
       document                                   // Update view
         .getElementById('display')               // Update view
@@ -26,10 +26,10 @@ for (let key of Object.keys(operations)) {
   document
     .getElementById(key)
     .addEventListener('click', function () {
-      operator = key;
       document
         .getElementById('current-calculation')
         .innerHTML = currentNumber + operations[key];
+      shouldClearDisplay = true;
     });
 }
 
