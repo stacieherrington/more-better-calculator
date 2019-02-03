bf3092ece5d7ccc196b4b4b6816d668a2e6a64ce
Jade Helmet <thejadehelmet@gmail.com>
2019-02-03 13:29:49 -0600

    We made plus and minus.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let currentNumber = 0;
+let operator;
+const operations = {
+  plus: " + ",
+  minus: " - ",
+};
+
+// operations.plus
+// operations["plus"]
+
+// const op = "minus";
+// operations[op]
 
 for (let i = 0; i < 10; i++) {
   document
@@ -8,6 +19,17 @@ for (let i = 0; i < 10; i++) {
       document                                   // Update view
         .getElementById('display')               // Update view
         .innerHTML = currentNumber;              // Update view
+    });
+}
+
+for (let key of Object.keys(operations)) {
+  document
+    .getElementById(key)
+    .addEventListener('click', function () {
+      operator = key;
+      document
+        .getElementById('current-calculation')
+        .innerHTML = currentNumber + operations[key];
     });
 }
 
